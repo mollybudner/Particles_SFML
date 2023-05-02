6d0fcd83b16b1ac3a66d35762096eeedbb6518a3
admin <admin@BCS-Ubuntu-02.lrccd.ad.losrios.edu>
2023-05-02 15:44:30 -0700

    ubuntu

--- Particles/Engine.cpp
+++ Particles/Engine.cpp
@@ -10,12 +10,17 @@ Engine()
 
 void run()
 {
-	Clock clock;
+	sf::Clock clock;
 	cout << "Starting Particle unit tests..." << endl;
 	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2,
 				  (int)m_Window.getSize().y / 2 });
 	p.unitTests();
 	cout << "Unit tests complete. Starting engine..." << endl;
+
+	while(m_Window.isOpen())
+	{
+		clock.restart();
+		float time = clock.getElapsedTime().asSeconds();
 }
 
 void input()
