2dae262da4ccf1290568b82213cbab68939d2e48
admin <admin@BCS-Ubuntu-02.lrccd.ad.losrios.edu>
2023-05-02 15:36:16 -0700

    ubuntu

--- Particles/Engine.cpp
+++ Particles/Engine.cpp
@@ -5,25 +5,30 @@ using namespace std;
 
 Engine()
 {
-
+	m_Window.create(VideoMode::getDesktopMode);
 }
 
-run()
+void run()
+{
+	Clock clock;
+	cout << "Starting Particle unit tests..." << endl;
+	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2,
+				  (int)m_Window.getSize().y / 2 });
+	p.unitTests();
+	cout << "Unit tests complete. Starting engine..." << endl;
+}
+
+void input()
 {
 
 }
 
-input()
+void update(float dtAsSeconds)
 {
 
 }
 
-update(float dtAsSeconds)
-{
-
-}
-
-draw()
+void draw()
 {
 
 }
