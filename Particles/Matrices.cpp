f7b5ce4cd0c5c2b7dc29a5a9eef44784731b5593
mollybudner <30453792+mollybudner@users.noreply.github.com>
2023-05-15 00:05:31 -0700

    Update Matrices.cpp

--- Particles/Matrices.cpp
+++ Particles/Matrices.cpp
@@ -103,3 +103,28 @@ namespace Matrices
 	}
 }
 
+Matrices::RotationMatrix::RotationMatrix(double theta) : Matrix(2,2)
+{
+	a[0][0] = cos(theta);
+	a[0][1] = -sin(theta);
+	a[1][0] = sin(theta);
+	a[1][1] = cos(theta);
+}
+
+Matrices::ScalingMatrix::ScalingMatrix(double scale) : Matrix(2, 2)
+{
+	a[0][0] = scale;
+	a[0][1] = 0;
+	a[1][0] = 0;
+	a[1][1] = scale;
+}
+
+Matrices::TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
+{
+	for (int i = 0; i < nCols; i++)
+	{
+		a.at(0).at(i) = xShift;
+		a.at(1).at(i) = yShift;
+	}
+}
+
